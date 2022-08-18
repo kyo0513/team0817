37ca002af3b5d25a248ff4d946b5cfccb7213cd2
kyo0513 <tyrant-zero-132127@docomo.ne.jp>
2022-08-18 23:20:55 +0900

    サンプル作成

--- Stage1/Scripts/Items.cs
+++ Stage1/Scripts/Items.cs
@@ -27,23 +27,16 @@ public class Items : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("ハンバーガー踏まれる");
+     if (other.gameObject.tag != "Stage"){
         //触れてきた対象（つまりネズミ）のRigidbody・animatorを取得します
-        Rigidbody2D player_rb       = other.gameObject.GetComponent<Rigidbody2D>();
-        Animator    player_animator = other.gameObject.GetComponent<Animator>();
-
+            Rigidbody2D player_rb       = other.gameObject.GetComponent<Rigidbody2D>();
+            Animator    player_animator = other.gameObject.GetComponent<Animator>();
         //ハンバーガーのアニメーション用のトリガーを付与します
-        animator.SetTrigger("IsAction");
-
+            animator.SetTrigger("IsAction");
         //プレイヤーのジャンプと同じ処理ですが1.2倍?飛ぶように設定しています
-        player_rb.AddForce(Vector2.up * jumpForce * 1.2f, ForceMode2D.Impulse);
-
-        //プレイヤーの見た目の変化のさせ方が不明；以下だとダメでした； ※新しいトリガーを追加することで解決
-        //player_animator.SetBool("IsJumping", true);
-        //player_animator.SetBool("IsFalling", false);
-
-        player_animator.SetTrigger("IsBownd");            
-        
+            player_rb.AddForce(Vector2.up * jumpForce * 1.2f, ForceMode2D.Impulse);
+            player_animator.SetTrigger("IsBownd");
+        }
     }
 
 }
